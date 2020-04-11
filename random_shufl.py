d2c9cbb92d202d9fb44ff30ee7d63666b29ca8bd
Vadász Zoltán <vadaszzoltan@minic.ro>
2020-04-11 23:53:00 +0300

    Még hiányzik a kiértékelés

--- random_shufl.py
+++ random_shufl.py
@@ -1,4 +1,6 @@
 import random
-
+print ('Ez egy kő / papír / olló játák')
+jatekos = input('Válassz:')
 lista = ["Kő", "Papír", "Olló"]
-print("A gép azt mondta, hogy: ", random.choice(lista))
+print ("A gép azt mondta, hogy: ", random.choice(lista)) 
+print ('Te azt választottad, hogy:', (jatekos))
