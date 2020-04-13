0ac6e808b3775c5550024a628e084416ec0354b5
Vadász Zoltán <vadaszzoltan@minic.ro>
2020-04-13 14:08:23 +0300

    Tündi a fejemre telepedett de egy rakás dolgot megértettem a végére

--- random_shufl.py
+++ random_shufl.py
@@ -1,6 +1,27 @@
 import random
-print ('Ez egy kő / papír / olló játák')
+
+def atalakitszobolszam(szo):
+    if szo == 'ko':
+        return 1
+    if szo == 'papir':
+        return 2
+    if szo == 'ollo':
+        return 3
+
+def atalakitszambolszo(szam):
+    if szam == 1:
+        return "ko"
+    if szam == 2:
+        return "papir"
+    if szam == 3:
+        return "ollo"
+
+lista = ["ko", "papir", "ollo"]
+
 jatekos = input('Válassz:')
-lista = ["Kő", "Papír", "Olló"]
-print ("A gép azt mondta, hogy: ", random.choice(lista)) 
-print ('Te azt választottad, hogy:', (jatekos))
+jatekosvalasz = atalakitszobolszam(jatekos)
+gep = random.choice(lista)
+print ("Gepvalasz:"+gep)
+gepvalasz = atalakitszobolszam(gep)
+print (jatekosvalasz - gepvalasz)
+
